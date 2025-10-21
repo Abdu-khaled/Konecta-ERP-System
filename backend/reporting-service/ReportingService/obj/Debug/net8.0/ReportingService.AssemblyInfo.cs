530fa2923f90742b5105d2797095cb275cf9c85f
Ahmed22Ayman <ahmed2107685@miuegypt.edu.eg>
2025-10-21 14:07:52 +0300

    reporting service

--- backend/reporting-service/ReportingService/obj/Debug/net8.0/ReportingService.AssemblyInfo.cs
+++ backend/reporting-service/ReportingService/obj/Debug/net8.0/ReportingService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportingService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d18bb3a9a333f7223f2d7d70c15c6967c4e3306")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21a606cd15226b78ea3086d7148c209bdf49f94d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportingService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReportingService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
